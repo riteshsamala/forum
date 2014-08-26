1179e9be2049a68f806851ad0159c09b07a64dde
Ritesh Samala <ritesh.samala@gmail.com>
2014-08-26 12:56:44 -0700

    testing

--- adm/create_forum.php
+++ adm/create_forum.php
@@ -3,17 +3,18 @@ define('IN_PHPBB', true);
 define('ADMIN_START', true);
 define('NEED_SID', true);
 // Include files
-$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : '../';
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : "../";
 echo $phpbb_root_path."is the root path";
 $phpEx = substr(strrchr(__FILE__, '.'), 1);
-
 echo "root path".$phpbb_root_path;
 require($phpbb_root_path . 'common.' . $phpEx);
-require($phpbb_root_path. 'adm/create_forum_function.' . $phpEx);
-require($phpbb_root_path. 'adm/create_post_function.' . $phpEx);
+echo "common included";
 require($phpbb_root_path . 'includes/functions_admin.' . $phpEx);
 require($phpbb_root_path . 'includes/functions_module.' . $phpEx);
 include($phpbb_root_path . 'includes/functions_posting.' . $phpEx);
+echo "includes finished";
+require($phpbb_root_path. 'adm/create_forum_function.' . $phpEx);
+require($phpbb_root_path. 'adm/create_post_function.' . $phpEx);
 
 
 
@@ -68,14 +69,14 @@ if ($forum_id)
 			echo "resource exists";
     		echo "redirect to new node topic";
     		//if yes, extract the topic_id and redirect to the topic under the forum
-			header( "Location: $next_url"."&t=".$res['topic_id']."&sid=".$sid_f );	
+			//header( "Location: $next_url"."&t=".$res['topic_id']."&sid=".$sid_f );	
 		}
 		else{
 			//if not create the topic
 			$return_url=create_node_topic($forum_id,$node_id,$node_desc);
 			print_r($return_url);
 	    	//Now, redirect to the topic inside the node
-		    header("Location: $return_url[redirect_to]");	
+		    //header("Location: $return_url[redirect_to]");	
 		}	
 	}
 	else{
@@ -83,7 +84,7 @@ if ($forum_id)
 	    $tid_a=$db->sql_query("select topic_id from phpbb_topics where forum_id='$forum_id' AND topic_title='general discussion'");
 		$tid=$db->sql_fetchrow($tid_a);
 	    $next_url=$next_url."&t=".$tid['topic_id']."sid=".$sid_f;
-	    header("Location: $next_url");
+	    //header("Location: $next_url");
 	}
     
  }
@@ -97,7 +98,7 @@ if($node_id && $node_desc){
 	
 			print_r($return_url);
 	    	//Now, redirect to the topic inside the node
-		    header("Location: $return_url[redirect_to]");
+		    //header("Location: $return_url[redirect_to]");
         }	
 	
 	else{
@@ -105,7 +106,7 @@ if($node_id && $node_desc){
 	    $tid_a=$db->sql_query("select topic_id from phpbb_topics where forum_id=$forum_id AND topic_title='general discussion'");
 		$tid=$db->sql_fetchrow($tid_a);
 	    $next_url=$next_url."&t=".$tid['topic_id']."sid=".$sid_f;
-	    header("Location: $next_url");
+	    //header("Location: $next_url");
 	}
 
 //redirect to newly created thread corresponding to the node_id
